383909a8703208c7a93d9bf8a1171b9278aa36e9
ChunLiang Wu <lucky0978218035@gmail.com>
2023-04-14 17:25:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import config 
+import os
 
 import torch
 import torch.optim as optim
 
 from utils import (
     load_checkpoint,
-    get_loaders
+    get_loaders,
+    plot_couple_examples
 )
 from loss import YoloLoss
 from model_build import YOLOv3
@@ -14,6 +16,7 @@ import config
 from dataset import create_train_test_csv
 
 def main():
+    os.makedirs('tuned_model', exist_ok=True)
     # create train and test csv files, with image and label paths
     create_train_test_csv(config.IMG_DIR, config.LABEL_DIR)
 
@@ -22,9 +25,9 @@ def main():
     optimizer = optim.Adam(
         model.parameters(), lr=config.LEARNING_RATE, weight_decay=config.WEIGHT_DECAY
     )
-    train_loader, test_loader = get_loaders(
-        train_csv_path=config.DATASET + "train.csv",
-        test_csv_path=config.DATASET + "test.csv",
+    train_loader, test_loader, _ = get_loaders(
+        train_csv_path=config.DATASET + "/train.csv",
+        test_csv_path=config.DATAAZSET + "/test.csv",
     )
 
     # load model if specified
@@ -43,9 +46,14 @@ def main():
         * torch.tensor(config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
     ).to(config.DEVICE)
 
-
+    # train model
     train_fn(train_loader, test_loader, model, optimizer, loss_fn, scaler, scaled_anchors)
 
+    # plot examples from test set
+    plot_couple_examples(
+        model, test_loader, config.CONF_THRESHOLD,
+        config.NMS_IOU_THRESH, config.ANCHORS, config.DEVICE
+    )
 
 if __name__ == '__main__':
     main()
